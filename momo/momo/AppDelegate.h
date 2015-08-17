6cdeb6bc84da6650b2722a25c15311bef8163455
雷小波 <MyMac@BGMac.local>
2015-08-18 00:43:48 +0800

    初始化项目

---
+++ momo/momo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  momo
+//
+//  Created by 雷小波 on 15/8/18.
+//  Copyright (c) 2015年 BG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ momo/momo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  momo
+//
+//  Created by 雷小波 on 15/8/18.
+//  Copyright (c) 2015年 BG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
